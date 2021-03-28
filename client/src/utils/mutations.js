9c2c5fab7c37ad69c8cb6936c77d6d1f1b9bdcb8
jbeedle19 <josh.beedle@gmail.com>
2021-03-28 14:34:31 -0400

    added mutations for login/add_user/save_book/delete_book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,57 @@
 import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql`
-
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
 `;
 
 export const ADD_USER = gql`
-
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation saveBook($bookId: String, $authors: [String], $description: String, $title: String, $image: String, $link: String) {
+        saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link) {
+            _id
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+    mutation deleteBook($bookId: String!) {
+        deleteBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+            }
+        }
+    }
 `;
